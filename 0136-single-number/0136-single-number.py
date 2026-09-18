d97b664baaeffa3d8ecb29559a886f98a3432d48
Sivnand V <sivnandnanduz@gmail.com>
2026-09-18 14:51:06 +0530

    Time: 0 ms (100%), Space: 21.1 MB (70.41%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,16 +1,21 @@
 class Solution:
     def singleNumber(self, nums: list[int]) -> int:
-        if len(nums)> (3 * 10 ** 4):
-            return 0
-        elif len(nums) == 1:
-            return nums[0] 
+        # if len(nums)> (3 * 10 ** 4):
+        #     return 0
+        # elif len(nums) == 1:
+        #     return nums[0] 
 
-        nums.sort()
+        # nums.sort()
 
-        for i in range(1,len(nums)-2):
-            if nums[i] != nums[i-1] and nums[i] != nums[i+1]:
-                return nums[i]
-        if nums[0] != nums[1]:
-            return nums[0]
-        elif nums[len(nums)-1] != nums[len(nums)-2]:
-            return nums[len(nums)-1]
+        # for i in range(1,len(nums)-2):
+        #     if nums[i] != nums[i-1] and nums[i] != nums[i+1]:
+        #         return nums[i]
+        # if nums[0] != nums[1]:
+        #     return nums[0]
+        # elif nums[len(nums)-1] != nums[len(nums)-2]:
+        #     return nums[len(nums)-1]
+
+        res = 0
+        for i in nums:
+            res ^= i
+        return res
