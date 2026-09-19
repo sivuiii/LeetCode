fb98a3a447bbcc888a41c0aed450f74abb92e682
Sivnand V <sivnandnanduz@gmail.com>
2026-09-19 10:12:27 +0530

    Time: 0 ms (100%), Space: 19.4 MB (7.56%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.py
+++ 3637-trionic-array-i/3637-trionic-array-i.py
@@ -9,27 +9,16 @@ class Solution:
                 elif f == 2 or f == 3:
                     f = 3
                 else :
-                    print('1',f)
                     return False
             elif nums[i+1] < nums[i] :
                 if f == 1 or f == 2:
                     f = 2
                 else :
-                    print('2',f)
                     return False
             else :
-                print('3',f)
                 return False
         if f == 3 :
             return True
         else :
-            print('4',f)
             return False
 
-        # inc = 0
-        # dec = 0 
-        # inc2 = 0
-
-        # for i in range(len(nums)-2) :
-        #     if nums(i+1) > nums(i) :
-        #         if
