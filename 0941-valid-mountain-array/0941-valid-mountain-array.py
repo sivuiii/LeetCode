6ec167e4aa756efb789f7efbd585d31d0ee63e6c
Sivnand V <sivnandnanduz@gmail.com>
2026-09-19 09:34:49 +0530

    Time: 7 ms (99.62%), Space: 20.5 MB (80.21%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -2,23 +2,22 @@ class Solution:
     
     def validMountainArray(self, arr: list[int]) -> bool:
         
-        inc = 0
-        dec = 0
+        f = 0
         
         for i in range(len(arr)-1) :
             if arr[i+1] > arr[i] :
-                if dec == 0:
-                    inc = 1
+                if f == 0 or f == 1:
+                    f = 1
                 else :
                     return False
             elif arr[i+1] < arr[i] :
-                if inc == 1 :
-                    dec = 1
+                if f == 1 or f == 2:
+                    f = 2
                 else :
                     return False
             else :
                 return False
-        if inc == 1 and dec == 1 :
+        if f == 2 :
             return True
         else :
             return False
